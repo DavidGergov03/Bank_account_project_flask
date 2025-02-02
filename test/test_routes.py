524785aa801e6bf059b6fe494b4f4a5a65cec4e8
DavidGergov <149818343+DavidGergov03@users.noreply.github.com>
2025-02-02 18:39:45 +0200

    Properly testing all html pages

--- test/test_routes.py
+++ test/test_routes.py
@@ -2,11 +2,31 @@ import unittest
 from app import app
 
 class TestFlaskRoutes(unittest.TestCase):
+
     def setUp(self):
+        """Set up the test client"""
+        app.config['TESTING'] = True
+        app.config['SESSION_TYPE'] = 'filesystem'
         self.client = app.test_client()
 
-    def test_home_route(self):
+    def test_home_page(self):
+        """Test if the homepage loads correctly"""
         response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_current_account_page(self):
+        """Test if the current account creation page loads"""
+        response = self.client.get('/current_account')
+        self.assertEqual(response.status_code, 200)
+
+    def test_savings_account_page(self):
+        """Test if the savings account creation page loads"""
+        response = self.client.get('/savings_account')
+        self.assertEqual(response.status_code, 200)
+
+    def test_exit_app(self):
+        """Test if the exit page loads"""
+        response = self.client.post('/exit')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
