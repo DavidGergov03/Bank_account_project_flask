bc6669732cdad0ca2e1b1de7857d9754d6dccf77
DavidGergov <149818343+DavidGergov03@users.noreply.github.com>
2025-02-02 18:36:43 +0200

    Implementing proper unittests

--- test/test_account.py
+++ test/test_account.py
@@ -1,16 +1,32 @@
 import unittest
-from app import Account
+from app import BankAccount
 
-class TestAccount(unittest.TestCase):
-    def test_deposit(self):
-        acc = Account(100)
-        acc.deposit(50)
-        self.assertEqual(acc.balance, 150)
+class TestBankAccount(unittest.TestCase):
 
-    def test_withdraw(self):
-        acc = Account(100)
-        acc.withdraw(40)
-        self.assertEqual(acc.balance, 60)
+    def test_create_current_account(self):
+        """Test creating a current account"""
+        account = BankAccount("12345678", 1000, password="securepass")
+        result = account.currents_account("12345678", 1000, "securepass")
+        expected = {
+            'type': 'Current',
+            'account_number': '12345678',
+            'balance': 1000,
+            'password': 'securepass'
+        }
+        self.assertEqual(result, expected)
+
+    def test_create_savings_account(self):
+        """Test creating a savings account with interest"""
+        account = BankAccount("87654321", 5000, 2.5, password="securepass")
+        result = account.savings_account("87654321", 5000, 2.5, "securepass")
+        expected = {
+            'type': 'Saving',
+            'account_number': '87654321',
+            'balance': 5000,
+            'interest_rate': 2.5,
+            'password': 'securepass'
+        }
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
